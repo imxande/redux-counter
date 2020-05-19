573d55646753a06fbc50d8a597ba5f3df97e30e1
imxande <alerodriguezlarosa@gmail.com>
2020-05-19 11:15:47 -0400

    counter renders

--- redux-counter/src/App.js
+++ redux-counter/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
+
 
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      This is fun!
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement(5))}>-</button>
+      {isLogged? <h3>Valuable info should not see</h3>: ""}
     </div>
   );
 }
